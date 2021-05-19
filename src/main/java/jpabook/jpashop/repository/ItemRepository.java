e3c4789bb57a0361686a82dc5ffdf6ae1fe6a92a
Bryce Yang <nash31@korea.ac.kr>
2021-05-19 17:21:33 +0900

    final commit

--- src/main/java/jpabook/jpashop/repository/ItemRepository.java
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -5,7 +5,9 @@ import java.util.List;
 import javax.persistence.EntityManager;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import lombok.RequiredArgsConstructor;
 
@@ -22,6 +24,14 @@ public class ItemRepository {
 		}
 
 		em.merge(item);
+	}
+
+	@Transactional
+	public void updateItem(Long itemId, Book param) {
+		Item findItem = findOne(itemId);
+		findItem.setPrice(param.getPrice());
+		findItem.setStockQuantity(param.getStockQuantity());
+		findItem.setName(param.getName());
 	}
 
 	public Item findOne(Long id) {
